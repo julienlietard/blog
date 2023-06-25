13e6c3e86403290e3aeb5d3aba14aefce41928f9
Julien Lietard <contact.julienlietard@gmail.com>
2023-06-25 20:02:17 +0200

    evol

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import './styles.css';
+import scientists from '../../../assets/gif/scientists.gif';
+import { AiFillCaretDown } from 'react-icons/ai';
 
+const scrollToBottom = () => {
+    window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+    });
+};
 const Header = () => (
   <header className='home-header'>
-    <h2>Inc. This Morning</h2>
+    <h5>Bienvenue dans mon</h5>
     <h1>
-      <span>“</span> Blog <span>”</span>
+      Labo
     </h1>
-    <p>
-      awesome place to make oneself <br /> productive and entertained through
-      daily updates.
-    </p>
+    <h5 className="text-light">
+        Vous y trouverez une variété d'articles captivants sur des sujets qui me passionnent<br/>
+        tels que l'Informatique, l'Astronomie et l'Astrophysique <span className="cursor">|</span>
+        <br/>
+        <a href="https://www.julienlietard.fr">Visiter mon portfolio</a>
+    </h5>
+      <a href={null} className="btn btn-primary" id="scrolldown" onClick={scrollToBottom}>Parcourir mes articles <AiFillCaretDown/></a>
+    <div className="illustration">
+        <img src={scientists} alt="illustration"/>
+    </div>
   </header>
 );
 
--- src/components/Home/SearchBar/index.jsx
+++ src/components/Home/SearchBar/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './styles.css';
+import {TiDeleteOutline} from 'react-icons/ti';
 
 const SearchBar = ({ formSubmit, value, handleSearchKey, clearSearch }) => (
   <div className='searchBar-wrap'>
     <form onSubmit={formSubmit}>
       <input
         type='text'
-        placeholder='Search By Category'
+        placeholder='astronomie'
         value={value}
         onChange={handleSearchKey}
       />
-      {value && <span onClick={clearSearch}>X</span>}
-      <button>Go</button>
+      {value && <span onClick={clearSearch} className="croix"><TiDeleteOutline/></span>}
+      <button>Rechercher</button>
     </form>
   </div>
 );
